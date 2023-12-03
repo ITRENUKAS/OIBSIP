9f8d85aed23e0e3918b69648daddb966973b05fe
ITRENUKAS <138301243+ITRENUKAS@users.noreply.github.com>
2023-12-03 12:48:28 +0530

    Random Password Generates-Project 3 Proposal
    Length and Character Types
    (letters,numbers,symbols)

---
+++ Random_Password_generator.py
@@ -0,0 +1,9 @@
+import random
+
+password ="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*"
+
+length_password = int(input("Enter the length of the password : "))
+
+a = "" .join(random.sample(password,length_password))
+
+print(f"your password is {a}")
